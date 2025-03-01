f85df3016ec773ddd85881019515a9c10d62b78f
tupe12334 <ofek.gabay.he@gmail.com>
2025-03-01 20:07:57 +0200

    Update Swagger documentation and save OpenAPI schema to file

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { writeFile } from 'fs/promises';
 import { patchNestJsSwagger } from 'nestjs-zod';
+import * as path from 'path';
 import { AppModule } from './app.module';
-import { version } from '../package.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,12 +13,18 @@ async function bootstrap() {
     .setDescription(
       "This is an API to run a local cache of the Security and exchange commission's API to prevent rate limiting and to provide a more stable and reliable API for the users",
     )
-    .setVersion(version)
+    .setVersion(process.env.npm_package_version)
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   const document = documentFactory();
-  SwaggerModule.setup('api', app, document);
 
+  // Save the Swagger OpenAPI schema to a file
+  const outputPath = path.join(__dirname, '..', 'swagger-spec.json');
+  console.log(`Writing OpenAPI schema to ${outputPath}`);
+
+  await writeFile(outputPath, JSON.stringify(document, null, 2));
+
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
